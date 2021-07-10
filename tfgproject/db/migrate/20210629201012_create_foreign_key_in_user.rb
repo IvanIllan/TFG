36c1a66d640ea243dcc28c460ec5f0e4b7219780
IvanIllan <32303859+IvanIllan@users.noreply.github.com>
2021-07-10 16:02:13 +0200

    Revert "44 new user table"

--- tfgproject/db/migrate/20210629201012_create_foreign_key_in_user.rb
+++
@@ -1,5 +0,0 @@
-class CreateForeignKeyInUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column("users","establishment_id","string")
-  end
-end  
--- tfgproject/db/schema.rb
+++ tfgproject/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_201012) do
+ActiveRecord::Schema.define(version: 2021_06_28_003546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,15 +43,6 @@ ActiveRecord::Schema.define(version: 2021_06_29_201012) do
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "establishment_id"
   end
 
   add_foreign_key "establishments", "companies"
