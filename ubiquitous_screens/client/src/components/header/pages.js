a14a131b54c3dbaa953da512d0e8edea6877878a
IvanIllan <ivanillanbarraya@gmail.com>
2024-05-02 21:07:00 +0200

    Manage the session of the user in the react app

--- ubiquitous_screens/client/src/components/header/pages.js
+++ ubiquitous_screens/client/src/components/header/pages.js
@@ -4,5 +4,9 @@ export default {
   public: [
     { id: 'signup', title: 'Sign up', path: routes.signup },
     { id: 'signin', title: 'Sign in', path: routes.signin }
+  ],
+  private: [
+    { id: 'screens', title: 'Configurador', path: routes.screens },
+    { id: 'items', title: 'Creador de Objetos', path: routes.items } 
   ]
 };
